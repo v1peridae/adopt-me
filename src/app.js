efeabec7c3a8dcb831fdcd481027ab594755bc0b
Louisa Mulei <louisakavindu@gmail.com>
2024-08-31 09:03:15 +0300

    feat: Add Pet component and update App component to display pets for adoption

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
 const App = () => {
-  return React.createElement("div", {}, React.createElement("h1", {}, "Adopt me!"));
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Giggleshitter",
+      breed: "Ginger",
+    }),
+    React.createElement(Pet, {
+      animal: "Parrot",
+      name: "Larry",
+      breed: "Parrotlet",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
